00e3cd01ec8d8f1092e30d400db3cc6e93c7f943
mateusz0909 <35651839+mateusz0909@users.noreply.github.com>
2019-01-13 11:18:33 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+document.getElementById('button').addEventListener('click', getJoke)
+
+function getJoke(e) {
+  //GET NUMBER FROM INPUT
+  const number = document.querySelector('input[type= "number"]').value
+  //JOKE REQUEST
+  const xhr = new XMLHttpRequest();
+  //GIF REQUEST
+  const xhr2 = new XMLHttpRequest();
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+  xhr2.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=funny', true)
+
+  //GET JOKE
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      let output = '';
+
+      if (response.type = "success") {
+        response.value.forEach(function (chuck) {
+            output += `<li>${chuck.joke}</li>`
+          }
+
+        )
+      }
+      document.querySelector('.jokes').innerHTML = output;
+    }
+
+  }
+//GET GIF
+  xhr2.onload = function () {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      let image = '';
+      if (response.meta.msg === 'OK') {
+        image = `<img src = "${response.data.image_original_url}" class="rounded img-fluid" alt="Responsive image">`;
+              }
+      document.querySelector('#image').innerHTML = image
+    }
+  }
+
+
+  xhr.send();
+  xhr2.send();
+
+  e.preventDefault();
+}
